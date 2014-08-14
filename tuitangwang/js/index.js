7b64a4bef4feb80de0e8466a313cb2228715167f
qianyi234 <1163167478@qq.com>
2014-08-14 08:51:22 +0800

    2014.8.14-01

--- tuitangwang/js/index.js
+++ tuitangwang/js/index.js
@@ -13,3 +13,52 @@ $(function(){
 		$(".dt-menu").hide();
 	});
 });
+
+var n=1;
+function showitem(){
+	/*改变大框的位置*/
+	$(".dt-slider-content").animate({"left":"-"+(n*100)+"%"},1000);
+	n++;
+	if(n>6){n=1;}
+}
+
+
+$(function(){
+	$(".dt-slider-point").click(function(){
+		showitem();
+		// //先将所有置为空
+		// $(".dt-slider-item").hide();
+		// $(".dt-slider-point").css({
+		// 	"background-position":"-978px -240px"
+		// });
+
+		// //为选中的项赋值
+		// var curTagId=$(this).attr("curId");
+		// var curPParent=$(this).parent().parent().parent().parent();
+		// //$(this).parent().parent().parent().parent().attr("curid",curTagId);
+		// curPParent.find(".item"+curTagId).show();
+		// $(this).css({
+		// 	"background-position":"-1002px -240px"
+		// });
+      });
+	});
+
+$(function(){
+	$(".dt-carousel-points").click(function(){
+		
+		//先将所有置为空
+		$(".dt-carousel-item").hide();
+		$(".dt-carousel-point").css({
+			"background-position":"-978px -240px"
+		});
+
+		//为选中的项赋值
+		var curTagId=$(this).attr("cId");
+		var curPParent=$(this).parent().parent().parent();
+		
+		curPParent.find(".it"+curTagId).show();
+		$(this).css({
+			"background-position":"-1002px -240px"
+		});
+      });
+	});
