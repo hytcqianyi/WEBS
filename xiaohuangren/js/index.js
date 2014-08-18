f9dc23a133db63684c364a6dbe731c62a42ff22d
qianyi234 <1163167478@qq.com>
2014-08-18 17:02:23 +0800

    2014.8.18-02
    renren改善and扣扣空间的标题

---
+++ xiaohuangren/js/index.js
@@ -0,0 +1,32 @@
+$(function(){
+        $(".basicintroA").hover(function(){
+                $(".logoA").css("background-position","-173px -81px");
+        },function(){
+                $(".logoA").css("background-position","");
+        });
+        $(".basicintroB").hover(function(){
+                $(".logoB").css("background-position","-173px -432px");
+        },function(){
+                $(".logoB").css("background-position","");
+        });
+        $(".basicintroC").hover(function(){
+                $(".logoC").css("background-position","-173px -167px");
+        },function(){
+                $(".logoC").css("background-position","");
+        });
+        $(".basicintroD").hover(function(){
+                $(".logoD").css("background-position","-173px -247px");
+        },function(){
+                $(".logoD").css("background-position","");
+        });
+        $(".basicintroE").hover(function(){
+                $(".logoE").css("background-position","-168px -346px");
+        },function(){
+                $(".logoE").css("background-position","");
+        });
+        $(".basicintroF").hover(function(){
+                $(".logoF").css("background-position","-173px 0px");
+        },function(){
+                $(".logoF").css("background-position","");
+        });
+});
