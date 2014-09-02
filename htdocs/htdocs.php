17306aa42e2fd5e9a7aa0e4274e309dab3606e64
qianyi234 <1163167478@qq.com>
2014-09-02 11:07:05 +0800

    2014.9.2-01
    php初步认识练习

---
+++ htdocs/htdocs.php
@@ -0,0 +1,20 @@
+
+<html>
+<head>
+	<meta charset="utf-8"/>
+
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<title></title>
+</head>
+<body>
+	<div id="all">
+		<ul class="allul">
+            <?php
+                  for($i=0;$i<10;$i++)
+                  	echo '<li class="newsA">母子跳河的入学悲剧怎样发生</li>';
+            ?>
+		</ul>
+	</div>
+</body>
+</html>
+
